63e35ea484ad7f8702c359f954f74245c4aeacdf
Danny Bimma <82052316+DannyBimma@users.noreply.github.com>
2022-04-05 12:21:08 -0400

    Initial Commit
    The skeleton of the project.

---
+++ p1-predictor/p1-predictor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  p1-predictor
+//
+//  Created by Daniel Trotman on 05/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
